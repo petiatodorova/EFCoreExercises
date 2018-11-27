68363a8755fbfbe5faee90f5d1a401ee3db48313
Petya Todorova <invisioncl@gmail.com>
2018-11-27 16:09:44 +0200

    Adding some functionalities with Linq.

--- MyFirstDB/MyFirstDB/Program.cs
+++ MyFirstDB/MyFirstDB/Program.cs
@@ -13,13 +13,67 @@ namespace MyFirstDB
             //-Provider Microsoft.EntityFrameworkCore.SqlServer -OutputDir Data
 
             var context = new MyFirstDbContext();
-            var friends = context
-                .Users
-                .Include(x => x.Friends)
-                .FirstOrDefault(x => x.Username == "Mimi")?
-                .Friends;
-            Console.WriteLine($"{string.Join(Environment.NewLine, friends.Select(y => y.Name))}");
-            Console.WriteLine("Hello World!");
+
+            // find all friends of some user
+            AllSomeonesFriends(context, "Koko");
+
+            // check if our table Friends is not empty
+            CheckIfAnyFriendsInDb(context);
+
+            // find the first friend of some user
+            FirstSomeonesFriend(context, "Jenny");
+
+        }
+
+        static void FirstSomeonesFriend(MyFirstDbContext context, string username)
+        {
+            var firstJennysFriend = context
+                            .Users
+                            .Include(x => x.Friends)
+                            .FirstOrDefault(x => x.Username == username)?
+                            .Friends;
+
+            if (firstJennysFriend == null || firstJennysFriend.FirstOrDefault() == null)
+            {
+                Console.WriteLine($"The user Jenny doesn't exists or she doen's have friends!");
+            }
+            else
+            {
+                Console.WriteLine($"The first Jenny's friend in our DB is --> {firstJennysFriend.FirstOrDefault().Name}");
+            }
+        }
+
+        // check if our table Friends is not empty
+        static void CheckIfAnyFriendsInDb(MyFirstDbContext context)
+        {
+            if (context.Friends.Any())
+            {
+                var findFirstFriend = context.Friends.Find(1).Name;
+                Console.WriteLine($"The first Friend in our DB is --> {findFirstFriend}");
+            }
+            else
+            {
+                Console.WriteLine($"There isn't any friends in our DB!");
+            }
+        }
+
+        // find all friends of some user
+        static void AllSomeonesFriends(MyFirstDbContext context, string username)
+        {
+            var allKokosFriends = context
+                            .Users
+                            .Include(x => x.Friends)
+                            .FirstOrDefault(x => x.Username == username)?
+                            .Friends;
+
+            if (allKokosFriends == null)
+            {
+                Console.WriteLine($"There is not such user!");
+            }
+            else
+            {
+                Console.WriteLine($"{string.Join(Environment.NewLine, allKokosFriends.Select(y => y.Name))}");
+            }
         }
     }
 }
