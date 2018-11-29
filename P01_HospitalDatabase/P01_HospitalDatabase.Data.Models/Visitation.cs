5cd604f824abfefc759f393711c98bb5960c2faa
Petya Todorova <invisioncl@gmail.com>
2018-11-29 20:41:58 +0200

    Hospital Database

---
+++ P01_HospitalDatabase/P01_HospitalDatabase.Data.Models/Visitation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Visitation
+    {
+        public int VisitationId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Comments { get; set; }
+
+        // navigation property Patient + Foreign Key PatientId
+        public int PatientId { get; set; }
+        public Patient Patient { get; set; }
+    }
+}
---
+++ P01_HospitalDatabase/P01_HospitalDatabase/Startup.cs
@@ -0,0 +1,12 @@
+﻿using P01_HospitalDatabase.Data;
+
+namespace P01_HospitalDatabase
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
