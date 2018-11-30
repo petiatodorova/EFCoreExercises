7e64c92f03fb213cbaeb4ac3d0b3c1b3041471d1
Petya Todorova <invisioncl@gmail.com>
2018-11-30 13:08:56 +0200

    Sales Database

---
+++ SalesDatabase/P03_SalesDatabase.Data/EntityConfiguration/SaleConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using P03_SalesDatabase.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.Data.EntityConfiguration
+{
+    public class SaleConfig : IEntityTypeConfiguration<Sale>
+    {
+        public void Configure(EntityTypeBuilder<Sale> builder)
+        {
+            builder.HasKey(x => x.SaleId);
+        }
+    }
+}
---
+++ SalesDatabase/P03_SalesDatabase/Startup.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace P03_SalesDatabase
+{
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
